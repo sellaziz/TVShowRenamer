7544ca854ff5995d395864228467aa510b6a02bb
Aziz S <sellaziz.code@outlook.fr>
2024-04-20 21:00:47 +0200

    all: adapt sources to fit usecase
    src: index.html: adapt to a two pane configuration
    src: main.js: set base to process input list
    src: style.css: format index.html to have two-pane
    src-tauri: add base Menu and disable greet function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn add_input(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_submenu(submenu);
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .menu(menu)
+        .on_menu_event(|event| {
+            match event.menu_item_id() {
+              "quit" => {
+                std::process::exit(0);
+              }
+              _ => {}
+            }
+          })
+        .invoke_handler(tauri::generate_handler![add_input])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,48 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+let inputInputEl;
+let outputListEl;
+let inputListEl;
 
-async function greet() {
+async function add_input() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  // inputMsgEl.textContent = await invoke("add_input", { name: inputInputEl.value });
+  if (inputInputEl.value != "") {
+    var li = document.createElement("li");
+    var t = document.createTextNode(inputInputEl.value);
+    li.appendChild(t);
+    inputListEl.appendChild(li);
+    inputInputEl.value = "";
+  }
+}
+
+async function process_input() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  // inputMsgEl.textContent = await invoke("add_input", { name: inputInputEl.value });
+  outputListEl.innerHTML = ""
+  for (const child of inputListEl.children) {
+    var li = document.createElement("li");
+    var t = document.createTextNode(child.innerText);
+    li.appendChild(t);
+    outputListEl.appendChild(li);
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  inputInputEl = document.querySelector("#input-input");
+  inputListEl = document.querySelector("#input-list");
+  outputListEl = document.querySelector("#output-list");
+  document.querySelector("#input-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    add_input();
   });
+  document.querySelector("#process-btn").onclick =
+    function() {
+      process_input();
+    }
+  ;
+  // document.querySelector("#content").addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   process_input();
+  // });
 });

3a0751def851e390d97612fbac145df3eff7ca5a
Aziz S <sellaziz.code@outlook.fr>
2024-04-20 22:52:09 +0200

    try to use file dialog

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,11 @@
 
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn add_input(name: &str) -> String {
@@ -11,7 +16,8 @@ fn add_input(name: &str) -> String {
 
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let open_file = CustomMenuItem::new("open_file".to_string(), "Open File...");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(open_file));
     let menu = Menu::new()
         .add_native_item(MenuItem::Copy)
         .add_submenu(submenu);
@@ -21,6 +27,9 @@ fn main() {
             match event.menu_item_id() {
               "quit" => {
                 std::process::exit(0);
+              }
+              "open_file" => {
+                event.window().emit("open_file", Payload { message: "msg".to_string() }).unwrap();
               }
               _ => {}
             }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const { invoke } = window.__TAURI__.tauri;
+import { open } from '@tauri-apps/api/dialog';
+import { emit, listen } from '@tauri-apps/api/event'
 
 let inputInputEl;
 let outputListEl;
@@ -19,13 +21,24 @@ async function add_input() {
 async function process_input() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   // inputMsgEl.textContent = await invoke("add_input", { name: inputInputEl.value });
-  outputListEl.innerHTML = ""
-  for (const child of inputListEl.children) {
-    var li = document.createElement("li");
-    var t = document.createTextNode(child.innerText);
-    li.appendChild(t);
-    outputListEl.appendChild(li);
-  }
+  // outputListEl.innerHTML = ""
+  // for (const child of inputListEl.children) {
+  //   var li = document.createElement("li");
+  //   var t = document.createTextNode(child.innerText);
+  //   li.appendChild(t);
+  //   outputListEl.appendChild(li);
+  // }
+  dialog.open({
+    directory: false,
+    multiple: false,
+    filter: { 'All Files': ['*'] }
+  })
+    .then((result) => {
+      console.log('Selected file:', result);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -41,6 +54,21 @@ window.addEventListener("DOMContentLoaded", () => {
       process_input();
     }
   ;
+  // const unlisten = await listen('open_file', (event) => {
+  //   // Open a selection dialog for directories
+  // const selected = await open({
+  //       directory: true,
+  //       multiple: true,
+  //       defaultPath: await appDir(),
+  //     });
+  //     if (Array.isArray(selected)) {
+  //       // user selected multiple directories
+  //     } else if (selected === null) {
+  //       // user cancelled the selection
+  //     } else {
+  //       // user selected a single directory
+  //     };
+  // });
   // document.querySelector("#content").addEventListener("submit", (e) => {
   //   e.preventDefault();
   //   process_input();
